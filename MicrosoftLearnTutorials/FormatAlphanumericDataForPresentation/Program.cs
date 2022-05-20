1898842472b428f50156240610ed0f1eda755da4
Tri Phan <phanphutri@gmail.com>
2022-05-20 08:12:08 +0700

    Message

---
+++ MicrosoftLearnTutorials/FormatAlphanumericDataForPresentation/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatAlphanumericDataForPresentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Challenge
+
+                // Output:
+                Dear Mr. Jones,
+                As a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.
+
+                Currently, you own 2,975,000.00 shares at a return of 12.75 %.
+
+                Our new product, Glorious Future offers a return of 13.13 %. Given your current volume, your potential profit would be ¤63,000,000.00.
+
+                Here's a quick comparison:
+
+                Magic Yield         12.75 %   ¤55,000,000.00      
+                Glorious Future     13.13 %   ¤63,000,000.00
+
+
+             */
+
+            string customerName = "Mr. Jones";
+
+            string currentProduct = "Magic Yield";
+            int currentShares = 2975000;
+            decimal currentReturn = 0.1275m;
+            decimal currentProfit = 55000000.0m;
+
+            string newProduct = "Glorious Future";
+            decimal newReturn = 0.13125m;
+            decimal newProfit = 63000000.0m;
+
+            // Your logic here
+            string message =
+                $"Dear {customerName}\n" +
+                $"As a customer of our {currentProduct} offering we are excited to tell you about " +
+                $"a new financial product that would dramatically increase your return.\n\n" +
+                $"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n\n" +
+                $"Our new product, {newProduct} offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C2}.\n";
+            Console.WriteLine(message);
+
+            Console.WriteLine("Here's a quick comparison:\n");
+            // Your logic here
+            string comparisonMesssage = "";
+            comparisonMesssage += currentProduct.PadRight(20);
+            comparisonMesssage += $"{currentReturn:P2}".PadRight(10);
+            comparisonMesssage += $"{currentProfit:C2}".PadLeft(10);
+            comparisonMesssage += "\n";
+            comparisonMesssage += newProduct.PadRight(20);
+            comparisonMesssage += $"{newReturn:P2}".PadRight(10);
+            comparisonMesssage += $"{newProfit:C2}".PadLeft(10);
+            Console.WriteLine(comparisonMesssage);
+        }
+
+        ////-----------------------------------------------------------------------------------------------
+        //static void Main(string[] args)
+        //{
+        //    /*
+        //        Exercise - Working with padded strings
+        //     */
+
+        //    string paymentID = "769";
+        //    string payeeName = "Mr. Stephen Ortega";
+        //    //string paymentAmount = "$5,000.00";
+        //    decimal amount = 5000m;
+        //    string paymentAmount = $"{amount:C}";
+
+
+        //    var formattedLine = paymentID.PadRight(6);
+        //    formattedLine += payeeName.PadRight(24);
+        //    formattedLine += paymentAmount.PadLeft(10);
+
+        //    Console.WriteLine("1234567890123456789012345678901234567890");
+        //    Console.WriteLine(formattedLine);
+        //}
+
+        ////-----------------------------------------------------------------------------------------------
+        //static void Main(string[] args)
+        //{
+        //    /*
+        //        Exercise - padding and alignment
+
+        //        Formatting strings by adding whitespace before and after
+        //        The PadLeft() method will add blank spaces to the left-hand side of the string
+        //        so that the total number of characters equals the argument you send it.
+
+        //        In this case, we want to total length of the string to be 12 characters.
+        //     */
+
+        //    string input = "Pad this";
+        //    Console.WriteLine(input.PadLeft(12));
+        //    //    Pad this
+
+        //    Console.Write(input.PadRight(12));
+        //    Console.WriteLine("hello");
+        //    //Pad this    hello
+
+        //    Console.WriteLine(input.PadLeft(12, '-'));
+        //    //----Pad this
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    /*
+        //        Exercise - string interpolation
+        //     */
+
+        //    int invoiceNumber = 1201;
+        //    decimal productMeasurement = 25.4568m;
+        //    decimal subtotal = 2750.00m;
+        //    decimal taxPercentage = .15825m;
+        //    decimal total = 3185.19m;
+
+        //    Console.WriteLine($"Invoice Number: {invoiceNumber}"); // 1201
+        //    Console.WriteLine($"    Measurement: {productMeasurement:N3}"); //  25.457
+        //    Console.WriteLine($"    Sub Total: {subtotal:C}"); //  $2,750.00
+        //    Console.WriteLine($"    Tax: {taxPercentage:P}"); //  15.83%
+        //    Console.WriteLine($"    Total Due: {total:C}");
+
+        //    // Output:
+        //    /*
+        //        Invoice Number: 1201
+        //            Measurement: 25.457
+        //            Sub Total: $2,750.00
+        //            Tax: 15.83%
+        //            Total Due: $3,185.19
+        //     */
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    /*
+        //        Composite Formating
+        //     */
+
+        //    //string first = "Hello";
+        //    //string second = "World";
+        //    ////string result = string.Format("{0} {1}!", first, second); // Hello World!
+        //    ////Console.WriteLine(result);
+        //    //Console.WriteLine(string.Format("{1} {0}", first, second)); // World Hello
+        //    //Console.WriteLine(string.Format("{0} {0} {0}", first, second)); // Hello Hello Hello
+
+        //    /*
+        //        String Interpolation
+        //     */
+
+        //    //string first = "Hello";
+        //    //string second = "World";
+        //    //Console.WriteLine($"{first} {second}!"); // Hello World!
+        //    //Console.WriteLine($"{second} {first}"); // World Hello
+        //    //Console.WriteLine($"{first} {first} {first}"); // Hello Hello Hello
+
+        //    /*
+        //        Formatting Currency
+        //        Composite formatting and string interpolation can be used to format values for
+        //        display given a specific languae and culture.
+
+        //        In the following example, the :C currency format specifier is used to present
+        //        the price and discount variables as currency.
+        //     */
+
+        //    //decimal price = 123.45m;
+        //    //int discount = 50;
+        //    //Console.WriteLine($"Price: {price:C}, (Save {discount:C})"); // Price: $123.45, (Save $50.00)
+
+        //    /*
+        //        Formatting Numbers
+        //        When working with numeric data, you may want to format the number for readability by
+        //        including commas to dilineate thousands, millions, billions, and so on.
+        //        The N numeric format specifier will do this.
+        //     */
+
+        //    //decimal measurement = 123456.78912m;
+        //    //Console.WriteLine($"Measurement: {measurement:N} units"); // Measurement: 123,456.79 units
+
+        //    /*
+        //        Note: By default, the N numeric format specifier displays only two digits after the decimal point.
+        //        If you want to display more precision, you can do that by adding a number after the specifier.
+
+        //        The following example will display four digits after the decimal point using the N4 specifier.
+        //     */
+
+        //    //decimal measurement = 123456.78912m;
+        //    //Console.WriteLine($"Measument: {measurement:N4} units"); // Measument: 123,456.7891 units
+
+        //    /*
+        //        Formatting Percentages
+        //        Use P format specifier to format percentages. Add a number afterwards to control the
+        //        number of values displayed after the decimal point.
+        //     */
+
+        //    decimal tax = .36785m;
+        //    Console.WriteLine($"Tax rate: {tax:P2}"); // Tax rate: 36.79%
+        //    tax += 0.1m;
+        //    Console.WriteLine($"Tax rate: {tax:P2}"); // Tax rate: 36.79%
+        //}
+    }
+}
---
+++ MicrosoftLearnTutorials/ModifyTheContentOfStringsTutorial/Program.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifyTheContentOfStringsTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Challenge
+             
+                In this challenge, you'll work with a string that contains fragment of HTML. You'll extract
+                data from the HTML fragment, replace some of its content, and remove other parts of its
+                content to achieve the desired output.
+             */
+
+            const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+            string quantity = "";
+            string output = "";
+
+            // Your work here
+
+            Console.WriteLine(quantity);
+            Console.WriteLine(output);
+
+            // Output:
+            // Quantity: 5000
+            // Output: <h2>Widgets &reg;</h2><span>5000</span>
+        }
+
+        // -----------------------------------------------------------------------------------------------------------
+        //static void Main(string[] args)
+        //{
+        //    /*
+        //        Exercise - Use the Remove() and Replace() methods.
+
+        //        In this exercise, we'll remove characters from a string using the Remove() method and
+        //        replace characters using the Replace() method.
+        //     */
+
+        //    /*
+        //        Step 1 - Write code to remove characters in specific location from a string
+
+        //        You would typically use Remove() when there's a standard and consistent position of the
+        //        characters you want to remove from the string.
+
+        //        Sometimes, data stored in older files is fixed length, where character positions are allocated
+        //        for certain fields of information. In this first step of our exercise, we have such fictitious
+        //        record of information. The first 5 digits represent a customer identification number. The
+        //        next 20 digits contain customer's name. The next 6 positions represent the customer latest
+        //        invoice amount, and the last three positions represent the number of items ordered on that invoice.
+
+        //        Perhaps we need to remove the customer's name to format the data so that it can be sent to
+        //        a seperate process. Since we know the exact position and legth of the user's name, we can 
+        //        easily remove it using the Remove() method.
+        //     */
+
+        //    //string data = "12345John Smith          5000  3  ";
+        //    //string updatedData = data.Remove(5, 20);
+        //    //Console.WriteLine(updatedData);
+        //    //// Output: 123455000  3
+        //    ///
+
+        //    /*
+        //        Step 2 - Write code to remove characters no matter where they appear in a string
+
+        //        You would use the Replace() method when you must replace one or more characters with a
+        //        different character (or no character). The Replace() method is different from the other
+        //        methods we've used so far inasmuch as it will replace every instance of the given characters,
+        //        not just the first or last instance.
+        //     */
+
+        //    string message = "This--is--ex-amp-le--da-ta";
+        //    message = message.Replace("--", " ");
+        //    message = message.Replace("-", "");
+        //    Console.WriteLine(message);
+        //    // Output: This is example data
+        //}
+
+        // -----------------------------------------------------------------------------------------------------------
+        //static void Main(string[] args)
+        //{
+        //    //string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+        //    string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol?";
+
+        //    char[] openingSymbols = { '(', '[', '{' };
+        //    int closingPosition = 0;
+        //    while (true)
+        //    {
+
+        //        int openingPosition = message.IndexOfAny(openingSymbols, closingPosition);
+        //        if (openingPosition == -1)
+        //        {
+        //            break;
+        //        }
+        //        string foundOpeningSymbol = message.Substring(openingPosition, 1);
+        //        char closingSymbol = ' ';
+        //        switch (foundOpeningSymbol)
+        //        {
+        //            case "(":
+        //                closingSymbol = ')';
+        //                break;
+        //            case "[":
+        //                closingSymbol = ']';
+        //                break;
+        //            case "{":
+        //                closingSymbol = '}';
+        //                break;
+        //        }
+        //        openingPosition++;
+        //        closingPosition = message.IndexOf(closingSymbol, openingPosition);
+        //        if ((openingPosition != -1) && (closingPosition != -1))
+        //        {
+        //            int length = closingPosition - openingPosition;
+        //            Console.WriteLine(message.Substring(openingPosition, length));
+        //        }
+        //        else
+        //        {
+        //            break;
+        //        }
+        //    }
+        //}
+
+
+        // -----------------------------------------------------------------------------------------------------------
+        //static void Main(string[] args)
+        //{
+        //    /*
+        //        Update the code example to work with different types of symbol sets
+
+        //        This time, we'll update the message string adding different types of symbols 
+        //        like square brackets and curly braces. We'll rely on IndexOfAny() to provide
+        //        an array of characters representing the opening symbols. IndexOfAny() will 
+        //        return us the first match it finds in the string.
+
+        //        Once we find a symbol, we'll need to find its matching closing symbol. Once
+        //        we do that, the rest should look similar. We'll use a different tatic instead
+        //        of modifying the original value of message. This time, we'll use the closing
+        //        position of the previous iteration as the opening position of the current iteration.
+        //     */
+
+        //    string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+        //    // The IndexOfAny() helper method requires a char array of characters.
+        //    // We want to look for:
+        //    char[] openSymbols = { '(', '[', '{' };
+
+        //    // We'll use a slightly different techniquefor iterating through the characters in the string.
+        //    // This time, we'll use the closing position of the previous iteration as the starting index
+        //    // for the next open symbol. So, we need to initialize the closingPosition variable to zero:
+        //    int closingPosition = 0;
+
+        //    while (true)
+        //    {
+        //        int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+        //        if (openingPosition == -1)
+        //        {
+        //            break;
+        //        }
+        //        string currentSymbol = message.Substring(openingPosition, 1);
+
+        //        // Now we must find the matching closing symbol
+        //        char matchingSymbol = ' ';
+        //        switch (currentSymbol)
+        //        {
+        //            case "(":
+        //                matchingSymbol = ')';
+        //                break;
+        //            case "[":
+        //                matchingSymbol = ']';
+        //                break;
+        //            case "{":
+        //                matchingSymbol = '}';
+        //                break;
+        //        }
+
+        //        // To find the closingPosition, we use an overload of the IndexOf method to specify
+        //        // that our search for the matchingSymbol should start at the openingPosition in the string.
+        //        openingPosition++;
+        //        closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+        //        int length = closingPosition - openingPosition;
+        //        Console.WriteLine(message.Substring(openingPosition, length));
+
+        //    }
+        //}
+
+        // -----------------------------------------------------------------------------------------------------------
+        //static void Main(string[] args)
+        //{
+        //    /*
+        //        Exercise - Use the string's IndexOf() and Substring() helper methods
+
+        //        In this exercise, you'll use the IndexOf() method and its variants including IndexOfAny() and
+        //        LastIndexOf() to locate the position of one character or string inside a larger string.
+
+        //        Once we've located the position, we can use the Substring() method to return the rest of the 
+        //        string after the position.
+
+        //        Or we can use an overloaded version of the Substring() method to set the number of characters (length)
+        //        to return after the position.
+        //     */
+
+        //    //// Step 1 - Write code to find an opening and closing parenthesis embedded in a string
+        //    //string message = "Find what is (inside the parentheses)";
+
+        //    //int openingPosition = message.IndexOf('('); // 13
+        //    //int closingPosition = message.IndexOf(')'); // 36
+
+        //    //Console.WriteLine(openingPosition);
+        //    //Console.WriteLine(closingPosition);
+
+        //    //// Step 2 - Add code to retrieve the value between two parenthesis characters
+        //    //openingPosition += 1;
+        //    //int length = closingPosition - openingPosition;
+        //    //Console.WriteLine(message.Substring(openingPosition, length)); // inside the parentheses
+
+
+        //    /*
+        //        The following snippet of code shows how to find the value inside an opening and closing <span> tag.
+        //     */
+
+        //    //string message = "What is the value <span>between the tags</span>?";
+
+        //    //const string openingSpan = "<span>";
+        //    //const string closingSpan = "</span>";
+
+        //    //int openingPosition = message.IndexOf(openingSpan); // 18
+        //    //int closingPostion = message.IndexOf(closingSpan); // 40
+
+        //    //openingPosition += openingSpan.Length;
+        //    //int length = closingPostion - openingPosition;
+        //    //Console.WriteLine(message.Substring(openingPosition, length)); // between the tags
+
+
+        //    /*
+        //        Write code to retrieve the last occurence of a sub string.
+        //     */
+
+        //    //string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+
+        //    //int openingPosition = message.LastIndexOf('(');
+        //    //openingPosition += 1;
+        //    //int closingPosition = message.LastIndexOf(')');
+        //    //int length = closingPosition - openingPosition;
+        //    //Console.WriteLine(message.Substring(openingPosition, length)); // set of parentheses
+
+
+        //    /*
+        //        Retrieve any value between one or more sets of parentheses in a string.
+        //        Way 1:
+        //     */
+
+        //    //string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+
+        //    //int beginningPosition = 0;
+        //    //while (beginningPosition != message.Length - 1)
+        //    //{
+
+        //    //    int openingPosition = message.IndexOf('(', beginningPosition);
+        //    //    int closingPosition = message.IndexOf(")", beginningPosition);
+        //    //    int length = closingPosition - openingPosition;
+        //    //    Console.WriteLine($"openingPosition = {openingPosition} - closingPosition = {closingPosition} - length = {length} - beginningPostion = {beginningPosition}");
+        //    //    Console.WriteLine(message.Substring(openingPosition + 1, length - 1));
+
+        //    //    beginningPosition = closingPosition + 1;
+        //    //}
+
+
+        //    /*
+        //        Retrieve any value between one or more sets of parentheses in a string.
+        //        Way 2:
+        //     */
+
+        //    string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+
+        //    while (true)
+        //    {
+        //        int openingPosition = message.IndexOf('(');
+        //        if (openingPosition == -1)
+        //        {
+        //            break;
+        //        }
+        //        openingPosition += 1;
+        //        int closingPosition = message.IndexOf(')');
+        //        int length = closingPosition - openingPosition;
+        //        Console.WriteLine(message.Substring(openingPosition, length));
+
+        //        // Note how we use the overload of the Substring() method to
+        //        // return only the remaining unprocessed message:
+        //        message = message.Substring(closingPosition + 1);
+        //    }
+        //}
+    }
+}
